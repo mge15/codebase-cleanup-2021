4c8b6bcd9d35135d218da789e40513393d8a9dcf
mge15 <77738774+mge15@users.noreply.github.com>
2021-04-04 21:09:31 -0400

    Testing ability to fetch

--- test/robo_test.py
+++ test/robo_test.py
@@ -1,4 +1,11 @@
 
 # TODO: import some code
 
+from app.robo import request_data
+
 # TODO: test the code
+
+def test_request_data():
+    keys = list(request_data("GOOGL").keys())
+    assert "Meta Data" in keys
+    assert "Time Series (Daily)" in keys
